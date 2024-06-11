e64b5a0e2bef112c2f6e2835ed47a1114ebdacc9
Anish Soni <anish.21cse189@rtu.ac.in>
2024-06-11 17:06:14 +0400

    initialised the project with react, tailwind and firebase

---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,9 @@
+export const Landing = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">
+        Welcome to the landing page Anish!
+      </p>
+    </div>
+  );
+};
