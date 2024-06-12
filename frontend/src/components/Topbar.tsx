dabbd3f886d956b7c8c927a1c79c24f8c6cc4c8c
Anish Soni <anish.21cse189@rtu.ac.in>
2024-06-12 16:59:18 +0400

    updated the navbar

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -2,10 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Topbar = () => {
   return (
-    <div className="max-w-screen-lg w-full bg-black align-center px-5 pb-5 pt-8">
-      {/* <img src="/logo.png" className="max-w-56" /> */}
       <NavBar />
-    </div>
   );
 };
 
@@ -30,15 +27,17 @@ const topbarItems = [
 
 function NavBar() {
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://localhost:5173" className="flex items-center space-x-3">
-          <img src="./vslogo.png" className="h-8 border" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Horse Code</span>
+          <img src="./vslogo.png" className="h-8" alt="Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900">Horse Code</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0">
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+          <button type="button" className="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
+            Get started
+          </button>
+          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -46,9 +45,11 @@ function NavBar() {
           </button>
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white">
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+              <Link to="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-indigo-600 md:p-0">
+                Home
+              </Link>
             </li>
             {topbarItems.map((item) => (
               <NavbarItem key={item.route} route={item.route} title={item.title} />
@@ -60,10 +61,10 @@ function NavBar() {
   );
 }
 
-function NavbarItem({ title, route } : { title: string , route: string }) {
+function NavbarItem({ title, route } : {title: string, route: string}) {
   return (
     <li>
-      <Link to={route} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+      <Link to={route} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-indigo-600 md:p-0">
         {title}
       </Link>
     </li>
